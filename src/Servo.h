3e958a4f07db69aed7fbce3b635205a4b9a2cf55
Jaroslav Paral <paral.jarek@gmail.com>
2023-08-29 19:37:31 +0200

    Change `Arduino.h` include

--- src/Servo.h
+++ src/Servo.h
@@ -24,7 +24,7 @@
  * SOFTWARE.
  *****************************************************************************/
 
- /*
+/*
  * Arduino srl - www.arduino.org
  * Base on lib for stm32f4 (d2a4a47): https://github.com/arduino-libraries/Servo/blob/master/src/stm32f4/ServoTimers.h
  * 2017 Jul 5: Edited by Jaroslav Páral (jarekparal) - paral@robotikabrno.cz
@@ -32,19 +32,20 @@
 
 #pragma once
 
-#include "Arduino.h"
+#include <Arduino.h>
 
-class Servo {
+class Servo
+{
     // Default min/max pulse widths (in microseconds) and angles (in
     // degrees).  Values chosen for Arduino compatibility.  These values
     // are part of the public API; DO NOT CHANGE THEM.
     static const int MIN_ANGLE = 0;
     static const int MAX_ANGLE = 180;
-    
-    static const int MIN_PULSE_WIDTH = 544;     // the shortest pulse sent to a servo
-    static const int MAX_PULSE_WIDTH = 2400;     // the longest pulse sent to a servo
+
+    static const int MIN_PULSE_WIDTH = 544;         // the shortest pulse sent to a servo
+    static const int MAX_PULSE_WIDTH = 2400;        // the longest pulse sent to a servo
     static const int MAX_COMPARE = ((1 << 16) - 1); // 65535
-    
+
     static const int TAU_MSEC = 20;
     static const int TAU_USEC = (TAU_MSEC * 1000);
 
@@ -55,7 +56,7 @@ public:
 
     // Pin number of unattached pins
     static const int PIN_NOT_ATTACHED = -1;
-    
+
     /**
      * @brief Construct a new Servo instance.
      *
@@ -70,42 +71,42 @@ public:
      */
     ~Servo();
 
-     /**
-     * @brief Associate this instance with a servomotor whose input is
-     *        connected to pin.
+    /**
+    * @brief Associate this instance with a servomotor whose input is
+    *        connected to pin.
 
-     * @param pin Pin connected to the servo pulse wave input. This
-     *            pin must be capable of PWM output (all ESP32 pins).
-     *
-     * @param channel Channel which is set to ESP32 Arduino function ledcSetup().
-     *                Channel must be number between 0 - 15.
-     *                It is possible to use automatic channel setup with constant
-     *                Servo::CHANNEL_NOT_ATTACHED.
-     * 
-     * @param minAngle Target angle (in degrees) associated with
-     *                 minPulseWidth.  Defaults to
-     *                 MIN_ANGLE = 0.
-     *
-     * @param maxAngle Target angle (in degrees) associated with
-     *                 maxPulseWidth.  Defaults to
-     *                 MAX_ANGLE = 180.
-     *  
-     * @param minPulseWidth Minimum pulse width to write to pin, in
-     *                      microseconds.  This will be associated
-     *                      with a minAngle degree angle.  Defaults to
-     *                      MIN_PULSE_WIDTH = 544.
-     *
-     * @param maxPulseWidth Maximum pulse width to write to pin, in
-     *                      microseconds.  This will be associated
-     *                      with a maxAngle degree angle. Defaults to
-     *                      MAX_PULSE_WIDTH = 2400.
-     *
-     * @sideeffect May set pinMode(pin, PWM).
-     *
-     * @return true if successful, false when pin doesn't support PWM.
-     */
-    bool attach(int pin, int channel = CHANNEL_NOT_ATTACHED, 
-                int minAngle = MIN_ANGLE, int maxAngle = MAX_ANGLE, 
+    * @param pin Pin connected to the servo pulse wave input. This
+    *            pin must be capable of PWM output (all ESP32 pins).
+    *
+    * @param channel Channel which is set to ESP32 Arduino function ledcSetup().
+    *                Channel must be number between 0 - 15.
+    *                It is possible to use automatic channel setup with constant
+    *                Servo::CHANNEL_NOT_ATTACHED.
+    *
+    * @param minAngle Target angle (in degrees) associated with
+    *                 minPulseWidth.  Defaults to
+    *                 MIN_ANGLE = 0.
+    *
+    * @param maxAngle Target angle (in degrees) associated with
+    *                 maxPulseWidth.  Defaults to
+    *                 MAX_ANGLE = 180.
+    *
+    * @param minPulseWidth Minimum pulse width to write to pin, in
+    *                      microseconds.  This will be associated
+    *                      with a minAngle degree angle.  Defaults to
+    *                      MIN_PULSE_WIDTH = 544.
+    *
+    * @param maxPulseWidth Maximum pulse width to write to pin, in
+    *                      microseconds.  This will be associated
+    *                      with a maxAngle degree angle. Defaults to
+    *                      MAX_PULSE_WIDTH = 2400.
+    *
+    * @sideeffect May set pinMode(pin, PWM).
+    *
+    * @return true if successful, false when pin doesn't support PWM.
+    */
+    bool attach(int pin, int channel = CHANNEL_NOT_ATTACHED,
+                int minAngle = MIN_ANGLE, int maxAngle = MAX_ANGLE,
                 int minPulseWidth = MIN_PULSE_WIDTH, int maxPulseWidth = MAX_PULSE_WIDTH);
 
     /**
@@ -155,7 +156,7 @@ public:
      * @see Servo::attach()
      */
     int readMicroseconds();
-    
+
     /**
      * @brief Check if this instance is attached to a servo.
      * @return true if this instance is attached to a servo, false otherwise.
@@ -171,14 +172,13 @@ public:
      */
     int attachedPin() const;
 
-
 private:
     void _resetFields(void);
 
-    int _usToDuty(int us)    { return map(us, 0, TAU_USEC, 0, MAX_COMPARE); }
-    int _dutyToUs(int duty)  { return map(duty, 0, MAX_COMPARE, 0, TAU_USEC); }
-    int _usToAngle(int us)   { return map(us, _minPulseWidth, _maxPulseWidth, _minAngle, _maxAngle); }
-    int _angleToUs(int angle){ return map(angle, _minAngle, _maxAngle, _minPulseWidth, _maxPulseWidth); }
+    int _usToDuty(int us) { return map(us, 0, TAU_USEC, 0, MAX_COMPARE); }
+    int _dutyToUs(int duty) { return map(duty, 0, MAX_COMPARE, 0, TAU_USEC); }
+    int _usToAngle(int us) { return map(us, _minPulseWidth, _maxPulseWidth, _minAngle, _maxAngle); }
+    int _angleToUs(int angle) { return map(angle, _minAngle, _maxAngle, _minPulseWidth, _maxPulseWidth); }
 
     static int channel_next_free;
 
