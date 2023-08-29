43a9e24d2108d4ff4e5dec25b31358cbb7c308e7
Jaroslav Paral <paral.jarek@gmail.com>
2023-08-29 19:33:05 +0200

    Try to change the file extension for GH CI

--- examples/01-SimpleServo/main.cpp
+++ examples/01-SimpleServo/main.cpp
@@ -4,19 +4,23 @@ static const int servoPin = 4;
 
 Servo servo1;
 
-void setup() {
+void setup()
+{
     Serial.begin(115200);
     servo1.attach(servoPin);
 }
 
-void loop() {
-    for(int posDegrees = 0; posDegrees <= 180; posDegrees++) {
+void loop()
+{
+    for (int posDegrees = 0; posDegrees <= 180; posDegrees++)
+    {
         servo1.write(posDegrees);
         Serial.println(posDegrees);
         delay(20);
     }
 
-    for(int posDegrees = 180; posDegrees >= 0; posDegrees--) {
+    for (int posDegrees = 180; posDegrees >= 0; posDegrees--)
+    {
         servo1.write(posDegrees);
         Serial.println(posDegrees);
         delay(20);
